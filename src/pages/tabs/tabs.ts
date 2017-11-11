1240bc200d1e03c8e0428a694f9d2dce8357a443
adimitui <adimitui@princeton.edu>
2017-11-11 07:43:58 -0500

    added good tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { CameraPage } from '../camera/camera';
+import { BookmarksPage } from '../bookmarks/bookmarks';
 
 @Component({
-  templateUrl: 'tabs.html'
+	selector: 'page-tabs',
+    templateUrl: 'tabs.html'
 })
+
 export class TabsPage {
+    cameraRoot = CameraPage;
+    bookmarksRoot = BookmarksPage;
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+    constructor() {
 
-  constructor() {
-
-  }
+    }
 }
